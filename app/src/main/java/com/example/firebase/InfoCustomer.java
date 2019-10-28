de3fcfcb6c8f3f435d72e7461494cc0d37c98e63
TuongNguyenX <44676265+TuongNguyenX@users.noreply.github.com>
2019-10-28 11:52:09 +0700

    Detele Login Register Test

--- app/src/main/java/com/example/firebase/InfoCustomer.java
+++ app/src/main/java/com/example/firebase/InfoCustomer.java
@@ -3,12 +3,16 @@ package com.example.firebase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class InfoCustomer extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_customer);
+
+
     }
 }
